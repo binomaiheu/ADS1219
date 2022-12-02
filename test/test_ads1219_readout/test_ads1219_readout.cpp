2b533f0b1540497764e5b997800e0927150d0b34
Bino Maiheu <binomaiheu@gmail.com>
2022-12-02 10:01:49 +0100

    added readout tests shorted and single shot

--- test/test_ads1219_readout/test_ads1219_readout.cpp
+++ test/test_ads1219_readout/test_ads1219_readout.cpp
@@ -5,6 +5,7 @@ ADS1219 adc;
 
 void setUp(void) {
     adc.begin();
+    adc.reset();
 }
 
 void tearDown(void) {
@@ -15,6 +16,32 @@ void test_ads1219_detect(void){
     TEST_ASSERT(adc.detect());
 }
 
+
+void test_ads1219_readShorted(void)
+{
+    uint8_t retcode;
+    int32_t value = adc.readShorted(&retcode);
+
+    TEST_ASSERT_EQUAL(0, retcode);
+
+    // TODO: add threshold for shorted ... if bias too large, faulty chip ??? what is a good value  ?
+}
+
+void test_ads1219_readSingleEnded(void)
+{
+    uint8_t retcode;
+    int32_t value;
+
+    // reset to default --> single shot mode
+    TEST_ASSERT_EQUAL(0, adc.reset());
+
+    for (uint8_t i = 0; i<4; i++)
+    {
+        value = adc.readSingleEnded(i, &retcode);
+        TEST_ASSERT_EQUAL(0, retcode);
+    }
+}
+
 void setup()
 {
     delay(2000);
@@ -22,6 +49,8 @@ void setup()
     UNITY_BEGIN();
     
     RUN_TEST(test_ads1219_detect);
+    RUN_TEST(test_ads1219_readShorted);
+    RUN_TEST(test_ads1219_readSingleEnded);
 
     UNITY_END();
 }
