fe37d80feaa7451213cdaf257bf0ec25b10f5eb3
Bino Maiheu <binomaiheu@gmail.com>
2022-12-02 09:39:00 +0100

    added vref test

--- test/test_ads1219_config/test_ads1219_config.cpp
+++ test/test_ads1219_config/test_ads1219_config.cpp
@@ -4,29 +4,35 @@
 
 ADS1219 adc;
 
-void setUp(void) {
+void setUp(void) 
+{
     adc.begin();
     adc.reset();
 }
 
-void tearDown(void) {
+void tearDown(void) 
+{
 }
 
-void test_ads1219_detect(void){
+void test_ads1219_detect(void)
+{
     // detect the device on the bus
     TEST_ASSERT(adc.detect());
 }
 
-void test_ads1219_reset(void){
+void test_ads1219_reset(void)
+{
     TEST_ASSERT_EQUAL(0, adc.reset());
 }
 
-void test_ads1219_read_gain(void){
+void test_ads1219_read_gain(void)
+{
     uint8_t gain;
     TEST_ASSERT_EQUAL(0, adc.getGain(&gain));
 }
 
-void test_ads1219_set_gain(void){
+void test_ads1219_set_gain(void)
+{
     uint8_t gain;
 
     // set gain to 1 & read back    
@@ -40,6 +46,26 @@ void test_ads1219_set_gain(void){
     TEST_ASSERT_EQUAL(ADS1219_GAIN_FOUR, gain);
 }
 
+void test_ads1219_read_vref(void)
+{
+    uint8_t type;
+    TEST_ASSERT_EQUAL(0, adc.getVREF(&type));
+}
+
+void test_ads1219_set_vref(void)
+{
+    uint8_t type;
+
+    TEST_ASSERT_EQUAL(0, adc.setVREF(ADS1219_VREF_EXTERNAL));
+    TEST_ASSERT_EQUAL(0, adc.getVREF(&type));
+    TEST_ASSERT_EQUAL(ADS1219_VREF_EXTERNAL, type);
+
+    TEST_ASSERT_EQUAL(0, adc.setVREF(ADS1219_VREF_INTERNAL));
+    TEST_ASSERT_EQUAL(0, adc.getVREF(&type));
+    TEST_ASSERT_EQUAL(ADS1219_VREF_INTERNAL, type);
+}
+
+
 
 void setup()
 {
@@ -51,6 +77,13 @@ void setup()
     RUN_TEST(test_ads1219_reset);
     RUN_TEST(test_ads1219_read_gain);
     RUN_TEST(test_ads1219_set_gain);
+    RUN_TEST(test_ads1219_read_vref);
+    RUN_TEST(test_ads1219_set_vref);
+
+
+    // Don't forget to make tests for the whole configuration structure and peform different operations in succession
+    // to see whether orring of bits is ok
+
 
     UNITY_END();
 }
