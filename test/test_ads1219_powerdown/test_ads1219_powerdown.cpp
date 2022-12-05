edf2a42d2022dad59ec2124ad950345585a932e6
Bino Maiheu <binomaiheu@gmail.com>
2022-12-05 13:32:51 +0100

    implemented powerdown test

--- test/test_ads1219_powerdown/test_ads1219_powerdown.cpp
+++ test/test_ads1219_powerdown/test_ads1219_powerdown.cpp
@@ -11,9 +11,36 @@ void tearDown(void) {
 }
 
 void test_ads1219_powerdown(void){
+    
+    uint8_t gain, vref, rate;
+
     // detect the device on the bus
     TEST_ASSERT(adc.detect());
-    TEST_FAIL_MESSAGE("Not implemented yet");
+
+    TEST_ASSERT_EQUAL_MESSAGE(0, adc.reset(), "error resetting");
+
+    // set some funky config values
+    TEST_ASSERT_EQUAL_MESSAGE(0, adc.setGain(ADS1219_GAIN_FOUR), "error setting gain");
+    TEST_ASSERT_EQUAL_MESSAGE(0, adc.setVREF(ADS1219_VREF_EXTERNAL), "error setting vref");
+    TEST_ASSERT_EQUAL_MESSAGE(0, adc.setDataRate(ADS1219_DATARATE_90SPS), "error setting datarate");
+
+    TEST_ASSERT_EQUAL_MESSAGE(0, adc.powerDown(), "error sending power down");
+
+    // sleep
+    delay(2000);
+
+    // issue start to wake up
+    TEST_ASSERT_EQUAL_MESSAGE(0, adc.start(), "error starting after power up");
+
+    // read back configuration
+    TEST_ASSERT_EQUAL_MESSAGE(0, adc.getGain(&gain), "error reading gain after power up" );
+    TEST_ASSERT_EQUAL_MESSAGE(0, adc.getVREF(&vref), "error reading vref after power up" );
+    TEST_ASSERT_EQUAL_MESSAGE(0, adc.getDataRate(&rate), "error reading datarate after power up" );
+
+    // check equal
+    TEST_ASSERT_EQUAL_MESSAGE( ADS1219_GAIN_FOUR, gain, "gain mismatch after power up" );
+    TEST_ASSERT_EQUAL_MESSAGE( ADS1219_VREF_EXTERNAL, vref, "vref type mismatch after power up" );
+    TEST_ASSERT_EQUAL_MESSAGE( ADS1219_DATARATE_90SPS, rate, "datarate mismatch after power up");
 }
 
 void setup()
